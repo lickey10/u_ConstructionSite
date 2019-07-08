0c63ef408b5b72085e200a7bdb87b9df65ecc6f3
lickey10 <lickey10@gmail.com>
2019-07-08 12:35:42 -0600

    first import

---
+++ ConstructionSite/Assets/Breakable Primary Jump Ramp/Scripts/Primary Jump Ramp_Controller.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+using System.Collections;
+
+public class Primary Jump Ramp_Controller : PhysicsController {
+}
---
+++ ConstructionSite/Assets/Breakable Primary Jump Ramp/Scripts/Primary Jump Ramp_DL1_Master.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+using System.Collections;
+
+public class Primary Jump Ramp_DL1_Master : PhysicsController_DL1_Master {
+}
